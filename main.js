121f9460bd506bacfc7000a014494b46f7140ecb
hammad rafiq2277 <hammadrafiq2277@gmail.com>
2026-04-07 23:17:16 +0500

    inserting and removing elements in js

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-let a=10;
-if(a>10){
-    console.log("successful")
-}
-else{
-    console.log("Try Again.....")
-}
+// document.querySelector(".container").outerHTML
+// '<div class="container">\n  <div class="box">hello i am a box</div>\n   <div class="box">hello i am a box</div>\n   </div>'
+// document.querySelector(".box").outerHTML
+// '<div class="box">hello i am a box</div>'
+// document.querySelector(".container").tagName
+// 'DIV'
+// document.querySelector(".box").tagName
+// 'DIV'
+// document.querySelector(".container").textContent
+// '\n        hello i am a box\n        hello i am a box\n    '
+// document.querySelector(".container").hidden
+// false
+// document.querySelector(".container").hidden=true
+// true
+// document.querySelector(".container").classList.toggle("carton")
+// false
+// document.querySelector(".container").classList.toggle("carton")
+// true
+
+
+let div=document.createElement("div");
+div.setAttribute("class","snd-box");
+div.innerHTML="This box is Inserted <b> through javascript</b>";
+document.querySelector(".container").append(div); //add at last of box.
+document.querySelector(".container").prepend(div); //add at start of box.
+document.querySelector(".container").before(div);  // add before container.
+document.querySelector(".container").after(div);   // add after container.
+
+let str=document.querySelector(".container");
+str.insertAdjacentHTML("beforebegin","<h1>hi i am html string </h1>");
+
